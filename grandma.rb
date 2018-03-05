99c1d2756ea72a28f290372458edf10813be89be
Katleiah <katleiahramos@gmail.com>
2018-03-05 21:02:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,11 +14,11 @@
 
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
-    return I LOVE YOU TOO PUMPKIN!
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif statement == statement.upcase
-    return NO, NOT SINCE 1938!
+    return "NO, NOT SINCE 1938!"
   else
-    return HUH?! SPEAK UP, SONNY!
+    return "HUH?! SPEAK UP, SONNY!"
   end
 
 end
